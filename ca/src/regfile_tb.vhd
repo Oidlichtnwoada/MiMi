9dc49065095b5a6b18c89c26e4577f23f7d98026
Hannes Brantner <e01614466@tuwien.ac.at>
2019-05-14 17:05:06 +0200

    beginning of the tb framework

--- ca/src/regfile_tb.vhd
+++ ca/src/regfile_tb.vhd
@@ -1,12 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
-use std.textio.all;
 use ieee.std_logic_textio.all;
+use std.textio.all;
 
 use work.core_pack.all;
 use work.regfile_pkg.all;
+use work.string_ops.all;
+use work.tb_pkg.all;
 
 entity regfile_tb is 
 end regfile_tb;
@@ -20,13 +21,40 @@ architecture rtl of regfile_tb is
     signal wrdata : std_logic_vector(DATA_WIDTH-1 downto 0);
     signal regwrite : std_logic;
 begin
+
     UUT: regfile
     port map (clk => clk, reset => reset, stall => stall, rdaddr1 => rdaddr1, rdaddr2 => rdaddr2, rddata1 => rddata1, rddata2 => rddata2, wraddr => wraddr, wrdata => wrdata, regwrite => regwrite);
 
     test: process
         file input_file : text;
-        variable l : line;
-
+        file output_file : text;
+        variable inputs : lines_t;
     begin
+        file_open(input_file, "input.txt", READ_MODE);
+        file_open(output_file, "simulated.txt", WRITE_MODE);
+        --
+        res <= '0';
+        wait for RESET_FACTOR * CLK_PERIOD;
+        res <= '1';
+        --
+        while not endfile(input_file) loop
+            clk <= '0';
+            wait for CLK_PERIOD/4;
+            --
+            readline(input_file, current_line);
+            inputs := split(current_line, ",");
+            stall <= inputs(0).all;
+            deallocate(inputs);
+            --
+            clk <= '1';
+            wait for CLK_PERIOD/2;
+            clk <= '0';
+            wait for CLK_PERIOD/4;
+            --read outputs
+        end loop;
+        file_close(input_file);
+        file_close(output_file);
+        wait;
     end process test;
+
 end rtl;
