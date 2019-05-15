4ad9c6c579372ea3dd93959bef7ac3a5ff6f5fd6
Hannes Brantner <e01614466@tuwien.ac.at>
2019-05-15 21:22:29 +0200

    start_test.sh works, the comparing comes with the next commit

--- ca/sim/regfile_sim/test.sh
+++
@@ -1,8 +0,0 @@
-# generate input.csv and output_expected.csv
-python3 ./generateinputandexpectedoutput.py 
-# compile sources
-make compile
-# simulate
-make sim
-# compare the result
-python3 ../../../python/csvcompare.py 
--- python/csvcompare.py
+++ python/csvcompare.py
@@ -1,1 +1,13 @@
-open('result.txt')
+class CSVCompare:
+    def __init__(self):
+        pass
+
+    def compareCSVs(self):
+        output_expected = open('output_expected.csv', 'r')
+        output_simulated = open('output_simulated.csv', 'r')
+        print(output_expected)
+        print(output_simulated)
+        output_expected.close()
+        output_simulated.close()
+
+CSVCompare().compareCSVs()
