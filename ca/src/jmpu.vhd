d0a6732afb2ba6bfa82f5e10393c7a6378ee8ed2
seiserbernhard <50596139+seiserbernhard@users.noreply.github.com>
2019-05-16 16:19:27 +0200

    jump unit coded but not yet tested

--- ca/src/jmpu.vhd
+++ ca/src/jmpu.vhd
@@ -16,4 +16,30 @@ architecture rtl of jmpu is
 
 begin  -- rtl
 
+	jump : process(op,N,Z)
+	begin
+	
+		case op is
+			when JMP_NOP =>
+				J <= '0';
+			when JMP_JMP =>
+				J <= '1';
+			when JMP_BEQ =>
+				J <= Z;
+			when JMP_BNE =>
+				J <= (not Z);
+			when JMP_BLEZ =>
+				J <= (N or Z);
+			when JMP_BGTZ =>
+				J <= (not (N or Z));
+			when JMP_BLTZ =>
+				J <= N;
+			when JMP_BGEZ =>
+				J <= (not N);
+			when others =>
+				J <= '0';
+		end case;
+	
+	end process;
+
 end rtl;
