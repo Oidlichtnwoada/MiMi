8bb34030990f4c74e1ea807f848adb8734336da8
Hannes Brantner <e01614466@tuwien.ac.at>
2019-05-19 17:59:04 +0200

    changes

--- ca/src/regfile.vhd
+++ ca/src/regfile.vhd
@@ -21,36 +21,43 @@ end regfile;
 architecture rtl of regfile is
 	type register_array is array (REG_COUNT-1 downto 0) of std_logic_vector(DATA_WIDTH-1 downto 0);
 	signal registers : register_array;
+	signal latched_rdaddr1 : std_logic_vector(REG_BITS-1 downto 0);
+	signal latched_rdaddr2 : std_logic_vector(REG_BITS-1 downto 0);
+	signal latched_wraddr : std_logic_vector(REG_BITS-1 downto 0); 
 begin
-	process(all)
+	process (all)
 	begin
-		if reset = '0' then
+		--writing
+		if regwrite = '1' then
+			registers(to_integer(unsigned(latched_wraddr))) <= wrdata;
+		end if;
+		--reading
+		if unsigned(latched_rdaddr1) = 0 then
 			rddata1 <= (others => '0');
+		elsif latched_rdaddr1 = latched_wraddr and regwrite = '1' then
+			rddata1 <= wrdata;
+		else 
+			rddata1 <= registers(to_integer(unsigned(latched_rdaddr1)));
+		end if;
+		if unsigned(latched_rdaddr2) = 0 then
 			rddata2 <= (others => '0');
-		elsif rising_edge(clk) then
-			if stall = '1' then
-				null;
-			else
-				--reading
-				if unsigned(rdaddr1) = 0 then
-					rddata1 <= (others => '0');
-				elsif rdaddr1 = wraddr and regwrite = '1' then
-					rddata1 <= wrdata;
-				else 
-					rddata1 <= registers(to_integer(unsigned(rdaddr1)));
-				end if;
-				if unsigned(rdaddr2) = 0 then
-					rddata2 <= (others => '0');
-				elsif rdaddr2 = wraddr and regwrite = '1' then
-					rddata2 <= wrdata;
-				else 
-					rddata2 <= registers(to_integer(unsigned(rdaddr2)));
-				end if;
-				--writing
-				if regwrite = '1' then
-					registers(to_integer(unsigned(wraddr))) <= wrdata;
-				end if;
-			end if;
+		elsif latched_rdaddr2 = latched_wraddr and regwrite = '1' then
+			rddata2 <= wrdata;
+		else 
+			rddata2 <= registers(to_integer(unsigned(latched_rdaddr2)));
 		end if;
 	end process;
+	
+	sync: process (all)
+	begin
+		if reset = '0' then 
+			latched_rdaddr1 <= (others => '0');
+			latched_rdaddr2 <= (others => '0');
+			latched_wraddr <= (others => '0');
+		elsif rising_edge(clk) and stall /= '1' then
+			latched_rdaddr1 <= rdaddr1;
+			latched_rdaddr2 <= rdaddr2;
+			latched_wraddr <= wraddr;
+		end if;
+	end process; 
 end rtl;
