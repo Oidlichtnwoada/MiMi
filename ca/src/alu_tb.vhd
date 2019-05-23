91d47a8772850df39c84f8d960044435f028f3cd
eliaseckenfellner <elias.eckenfellner@gmail.com>
2019-05-23 13:58:36 +0200

    Revert "Revert "Merge branch 'master' of https://github.com/Oidlichtnwoada/MiMi""
    This reverts commit 3fbaa67851659a5207e1fd3fac66fb2aaa180c48.

--- ca/src/alu_tb.vhd
+++ ca/src/alu_tb.vhd
@@ -1,6 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_textio.all;
+use std.textio.all;
 
 use work.core_pack.all;
 use work.op_pack.all;
@@ -28,19 +30,46 @@ begin
 		V => V
 	);
 	
---	test: process
---		file input_file : text;
---		file output_file : text;
---		variable current_read_line : line;
---		variable current_write_line : line;
---		variable inputs : lines_t;
---   begin
---	 
---		file_open(input_file, "input.csv", READ_MODE);
---      file_open(output_file, "output_simulated.csv", WRITE_MODE);
---		
---		
---	 
---	end process;
+	test: process
+		file input_file : text;
+		file output_file : text;
+		variable current_read_line : line;
+		variable current_write_line : line;
+		variable inputs : lines_t;
+   begin
+	 
+		file_open(input_file, "input.csv", READ_MODE);
+      file_open(output_file, "output_simulated.csv", WRITE_MODE);
+		
+		--creating header for output_simulated.csv
+      write(current_write_line, string'("R,Z,V"));
+      writeline(output_file, current_write_line);
+      --skip header from input.csv
+      readline(input_file, current_read_line);
+		
+		
+		while not endfile(input_file) loop
+			--assigning the inputs from the file
+			readline(input_file, current_read_line);
+			inputs := str_split(current_read_line.all, ",");
+			op <= str_to_slv(inputs(0).all);
+			A <= str_to_slv(inputs(1).all);
+			B <= str_to_slv(inputs(2).all);
+			deallocate(inputs);
+			
+			wait for CLK_PERIOD/2;
+
+			--read the output pins and write to output_simulated.csv
+			write(current_write_line, to_string(rddata1) & "," & to_string(rddata2));
+			writeline(output_file, current_write_line);
+			
+			wait for CLK_PERIOD/2;
+			
+		end loop;
+		file_close(input_file);
+		file_close(output_file);
+		wait;
+	 
+	end process;
 
 end architecture;
