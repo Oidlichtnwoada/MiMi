3c2070b35e5988f30596bacee3bea53bd2ace464
eliaseckenfellner <elias.eckenfellner@gmail.com>
2019-05-25 00:57:38 +0200

    mem_tb works

--- ca/src/memu_tb.vhd
+++ ca/src/memu_tb.vhd
@@ -32,6 +32,9 @@ begin
     begin
         file_open(input_file, "input.csv", READ_MODE);
         file_open(output_file, "output_simulated.csv", WRITE_MODE);
+        --creating header for output_simulated.csv
+        write(current_write_line, string'("M.address,M.rd,M.wr,M.byteena,M.wrdata,R,XL,XS"));
+        writeline(output_file, current_write_line);
         --skip header from input.csv
         readline(input_file, current_read_line);
         while not endfile(input_file) loop
