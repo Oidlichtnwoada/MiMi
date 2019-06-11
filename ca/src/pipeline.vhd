3bdfad809cb28a5fdfa66ebd018e300210e627ce
Hannes Brantner <e01614466@tuwien.ac.at>
2019-06-11 10:05:13 +0200

    adjusted modelsim project to new needs

--- ca/src/pipeline.vhd
+++ ca/src/pipeline.vhd
@@ -19,10 +19,10 @@ architecture rtl of pipeline is
 		port (
 			clk, reset : in	 std_logic;
 			stall      : in  std_logic;
-			pcsrc	   : in	 std_logic;
-			pc_in	   : in	 std_logic_vector(PC_WIDTH-1 downto 0);
+			pcsrc	  	 : in	 std_logic;
+			pc_in	   	 : in	 std_logic_vector(PC_WIDTH-1 downto 0);
 			pc_out	   : out std_logic_vector(PC_WIDTH-1 downto 0);
-			instr	   : out std_logic_vector(INSTR_WIDTH-1 downto 0)
+			instr	     : out std_logic_vector(INSTR_WIDTH-1 downto 0)
 		);
 	end component;
 
@@ -32,7 +32,7 @@ architecture rtl of pipeline is
 			stall      : in  std_logic;
 			flush      : in  std_logic;
 			pc_in      : in  std_logic_vector(PC_WIDTH-1 downto 0);
-			instr	   : in  std_logic_vector(INSTR_WIDTH-1 downto 0);
+			instr	   	 : in  std_logic_vector(INSTR_WIDTH-1 downto 0);
 			wraddr     : in  std_logic_vector(REG_BITS-1 downto 0);
 			wrdata     : in  std_logic_vector(DATA_WIDTH-1 downto 0);
 			regwrite   : in  std_logic;
@@ -45,20 +45,20 @@ architecture rtl of pipeline is
 			exc_dec    : out std_logic
 		);
 	end component;
-	
+
 	component exec is
 		port (
 			clk, reset       : in  std_logic;
-			stall      		 : in  std_logic;
+			stall      		   : in  std_logic;
 			flush            : in  std_logic;
 			pc_in            : in  std_logic_vector(PC_WIDTH-1 downto 0);
 			op	   	         : in  exec_op_type;
 			pc_out           : out std_logic_vector(PC_WIDTH-1 downto 0);
 			rd, rs, rt       : out std_logic_vector(REG_BITS-1 downto 0);
-			aluresult	     : out std_logic_vector(DATA_WIDTH-1 downto 0);
+			aluresult	       : out std_logic_vector(DATA_WIDTH-1 downto 0);
 			wrdata           : out std_logic_vector(DATA_WIDTH-1 downto 0);
 			zero, neg        : out std_logic;
-			new_pc           : out std_logic_vector(PC_WIDTH-1 downto 0);		
+			new_pc           : out std_logic_vector(PC_WIDTH-1 downto 0);
 			memop_in         : in  mem_op_type;
 			memop_out        : out mem_op_type;
 			jmpop_in         : in  jmp_op_type;
@@ -73,7 +73,7 @@ architecture rtl of pipeline is
 			exc_ovf          : out std_logic
 		);
 	end component;
-	
+
 	component mem is
 		port (
 			clk, reset    : in  std_logic;
@@ -101,7 +101,7 @@ architecture rtl of pipeline is
 			exc_store     : out std_logic
 		);
 	end component;
-	
+
 	component wb is
 		port (
 			clk, reset : in  std_logic;
@@ -150,45 +150,45 @@ architecture rtl of pipeline is
 	signal mem_wb_aluresult : std_logic_vector(DATA_WIDTH-1 downto 0);
 	signal mem_wb_memresult : std_logic_vector(DATA_WIDTH-1 downto 0);
 	signal mem_wb_wb_op : wb_op_type;
-	
+
 begin
 
 	flush <= '0';
 	stall <= mem_in.busy;
 
 	fetch_inst: fetch
-	port map(clk => clk, reset => reset, stall => stall, pcsrc => mem_fetch_pcsrc, 
+	port map(clk => clk, reset => reset, stall => stall, pcsrc => mem_fetch_pcsrc,
 				pc_in => mem_fetch_new_pc, pc_out => fetch_decode_pc, instr => fetch_decode_instr);
 
 	decode_inst: decode
-	port map(clk => clk, reset => reset, stall => stall, flush => flush, pc_in => fetch_decode_pc, 
-				instr => fetch_decode_instr, wraddr => wb_decode_wraddr, wrdata => wb_decode_wrdata, 
-				regwrite => wb_decode_regwrite, pc_out => decode_exec_pc, exec_op => decode_exec_exec_op, 
-				cop0_op => open, jmp_op => decode_exec_jmp_op, mem_op => decode_exec_mem_op, 
+	port map(clk => clk, reset => reset, stall => stall, flush => flush, pc_in => fetch_decode_pc,
+				instr => fetch_decode_instr, wraddr => wb_decode_wraddr, wrdata => wb_decode_wrdata,
+				regwrite => wb_decode_regwrite, pc_out => decode_exec_pc, exec_op => decode_exec_exec_op,
+				cop0_op => open, jmp_op => decode_exec_jmp_op, mem_op => decode_exec_mem_op,
 				wb_op => decode_exec_wb_op, exc_dec => open);
 
 	exec_inst: exec
-	port map(clk => clk, reset => reset, stall => stall, flush => flush, pc_in => decode_exec_pc, 
-				op => decode_exec_exec_op, pc_out => exec_mem_pc, rd => exec_mem_rd, rs => exec_mem_rs, 
-				rt => exec_mem_rt, aluresult => exec_mem_aluresult, wrdata => exec_mem_wrdata, 
-				zero => exec_mem_zero, neg => exec_mem_neg, new_pc => exec_mem_new_pc, 
-				memop_in => decode_exec_mem_op, memop_out => exec_mem_mem_op, jmpop_in => decode_exec_jmp_op, 
-				jmpop_out => exec_mem_jmp_op, wbop_in => decode_exec_wb_op, wbop_out => exec_mem_wb_op, 
-				forwardA => FWD_NONE, forwardB => FWD_NONE, cop0_rddata => (others => '0'), 
+	port map(clk => clk, reset => reset, stall => stall, flush => flush, pc_in => decode_exec_pc,
+				op => decode_exec_exec_op, pc_out => exec_mem_pc, rd => exec_mem_rd, rs => exec_mem_rs,
+				rt => exec_mem_rt, aluresult => exec_mem_aluresult, wrdata => exec_mem_wrdata,
+				zero => exec_mem_zero, neg => exec_mem_neg, new_pc => exec_mem_new_pc,
+				memop_in => decode_exec_mem_op, memop_out => exec_mem_mem_op, jmpop_in => decode_exec_jmp_op,
+				jmpop_out => exec_mem_jmp_op, wbop_in => decode_exec_wb_op, wbop_out => exec_mem_wb_op,
+				forwardA => FWD_NONE, forwardB => FWD_NONE, cop0_rddata => (others => '0'),
 				mem_aluresult => mem_wb_aluresult, wb_result => (others => '0'), exc_ovf => open);
 
 	mem_inst: mem
-	port map(clk => clk, reset => reset, stall => stall, flush => flush, 
-				mem_op => exec_mem_mem_op, jmp_op => exec_mem_jmp_op, pc_in => exec_mem_pc, rd_in => exec_mem_rd, 
-				aluresult_in => exec_mem_aluresult, wrdata => exec_mem_wrdata, zero => exec_mem_zero, 
-				neg => exec_mem_neg, new_pc_in => exec_mem_new_pc, pc_out => open, pcsrc => mem_fetch_pcsrc, 
-				rd_out => mem_wb_rd, aluresult_out => mem_wb_aluresult, memresult => mem_wb_memresult, 
-				new_pc_out => mem_fetch_new_pc, wbop_in => exec_mem_wb_op, wbop_out => mem_wb_wb_op, 
+	port map(clk => clk, reset => reset, stall => stall, flush => flush,
+				mem_op => exec_mem_mem_op, jmp_op => exec_mem_jmp_op, pc_in => exec_mem_pc, rd_in => exec_mem_rd,
+				aluresult_in => exec_mem_aluresult, wrdata => exec_mem_wrdata, zero => exec_mem_zero,
+				neg => exec_mem_neg, new_pc_in => exec_mem_new_pc, pc_out => open, pcsrc => mem_fetch_pcsrc,
+				rd_out => mem_wb_rd, aluresult_out => mem_wb_aluresult, memresult => mem_wb_memresult,
+				new_pc_out => mem_fetch_new_pc, wbop_in => exec_mem_wb_op, wbop_out => mem_wb_wb_op,
 				mem_out => mem_out, mem_data => mem_in.rddata, exc_load => open, exc_store => open);
 
 	wb_inst: wb
-	port map(clk => clk, reset => reset, stall => stall, flush => flush, op => mem_wb_wb_op, 
-				rd_in => mem_wb_rd, aluresult => mem_wb_aluresult, memresult => mem_wb_memresult, 
+	port map(clk => clk, reset => reset, stall => stall, flush => flush, op => mem_wb_wb_op,
+				rd_in => mem_wb_rd, aluresult => mem_wb_aluresult, memresult => mem_wb_memresult,
 				rd_out => wb_decode_wraddr, result => wb_decode_wrdata, regwrite => wb_decode_regwrite);
-	
+
 end rtl;
